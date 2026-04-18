634b3486ea50c6661727a1d1436411dd6a9c3b61
Sean Murphy <sean@iamseanmurphy.com>
2026-04-18 21:22:42 +0000

    3.9.12

--- customize-my-account-for-woocommerce/include/admin/wrap/endpoints-content.php
+++ customize-my-account-for-woocommerce/include/admin/wrap/endpoints-content.php
@@ -180,56 +180,72 @@ $default_color_font = '#334155';
 
 <?php include('subwrap/countof-settings.php'); ?>
 
- <tr>
-                    <td>
-                        <label class="wcmamtx_accordion_label"><?php  echo esc_html__('Icon Settings','customize-my-account-for-woocommerce'); ?></label>
-                    </td>
-                    <td>
-                        <?php 
-                             if (isset($value['icon_source']) && ($value['icon_source'] != '')) {
-                                $icon_source = $value['icon_source'];
-                             } else {
-                                $icon_source = 'default';
-                             }
-                        ?>
+<tr>
+    <td>
+        <label class="wcmamtx_accordion_label"><?php  echo esc_html__('Icon Settings','customize-my-account-for-woocommerce'); ?></label>
+    </td>
+    <td>
+       <?php 
+       if (isset($value['icon_source']) && ($value['icon_source'] != '')) {
+          $icon_source = $value['icon_source'];
+      } else {
+          $icon_source = 'default';
+      }
+      ?>
 
-                        <div class="wcmamtx_icon_settings_div">
-                            <div class="form-check wcmamtx_icon_checkbox">
-                                <input class="form-check-input wcmamtx_icon_source_radio" type="radio" name="<?php  echo esc_attr($this->wcmamtx_notices_settings_page); ?>[<?php echo $key; ?>][icon_source]"  value="default" <?php if ($icon_source == "default") { echo 'checked'; } ?>>
-                                <label class="form-check-label wcmamtx_icon_checkbox_label" >
-                                    <?php  echo esc_html__('Default Icon','customize-my-account-for-woocommerce'); ?>
-                                </label>
-                            </div>
-                            <div class="form-check wcmamtx_icon_checkbox">
-                                <input class="form-check-input wcmamtx_icon_source_radio" type="radio" name="<?php  echo esc_html__($this->wcmamtx_notices_settings_page); ?>[<?php echo $key; ?>][icon_source]"  value="noicon" <?php if ($icon_source == "noicon") { echo 'checked'; } ?>>
-                                <label class="form-check-label wcmamtx_icon_checkbox_label">
-                                    <?php  echo esc_html__('No Icon','customize-my-account-for-woocommerce'); ?>
-                                </label>
-                            </div>
-                            <div class="form-check wcmamtx_icon_checkbox">
-                                <input class="form-check-input wcmamtx_icon_source_radio" type="radio" name="<?php  echo esc_html__($this->wcmamtx_notices_settings_page); ?>[<?php echo $key; ?>][icon_source]"  value="custom" <?php if ($icon_source == "custom") { echo 'checked'; } ?>>
-                                <label class="form-check-label wcmamtx_icon_checkbox_label">
-                                    <?php  echo esc_html__('Font Awesome Icon','customize-my-account-for-woocommerce'); ?>
-                                </label>
-                            </div>
+      <div class="wcmamtx_icon_settings_div">
+          <div class="form-check wcmamtx_icon_checkbox">
+             <input class="form-check-input wcmamtx_icon_source_radio" type="radio" name="<?php  echo $this->wcmamtx_notices_settings_page; ?>[<?php echo $key; ?>][icon_source]"  value="default" <?php if ($icon_source == "default") { echo 'checked'; } ?>>
+             <label class="form-check-label wcmamtx_icon_checkbox_label" >
+                <?php  echo esc_html__('Default Icon','customize-my-account-for-woocommerce'); ?>
+            </label>
+        </div>
+        <div class="form-check wcmamtx_icon_checkbox">
+         <input class="form-check-input wcmamtx_icon_source_radio" type="radio" name="<?php  echo $this->wcmamtx_notices_settings_page; ?>[<?php echo $key; ?>][icon_source]"  value="default" <?php if ($icon_source == "noicon") { echo 'checked'; } ?> disabled>
+         <label class="form-check-label wcmamtx_icon_checkbox_label">
+            <?php  echo esc_html__('No Icon','customize-my-account-for-woocommerce'); ?>
+            <a href="#" data-toggle="modal" data-target="#wcmamtx_upgrade_modal">
+                <span class="wcmamtx_pro_upgrade_text"><?php  echo esc_html__('Upgrade to pro','customize-my-account-for-woocommerce'); ?></span>
 
-                            <div class="form-check wcmamtx_icon_checkbox">
-                                <input class="form-check-input wcmamtx_icon_source_radio" type="radio" name="<?php  echo esc_html__($this->wcmamtx_notices_settings_page); ?>[<?php echo $key; ?>][icon_source]"  value="dashicon" <?php if ($icon_source == "dashicon") { echo 'checked'; } ?>>
-                                <label class="form-check-label wcmamtx_icon_checkbox_label">
-                                    <?php  echo esc_html__('Dashicon','customize-my-account-for-woocommerce'); ?>
-                                </label>
-                            </div>
+            </a>
+        </label>
+    </div>
+    <div class="form-check wcmamtx_icon_checkbox">
+     <input class="form-check-input wcmamtx_icon_source_radio" type="radio" name="<?php  echo $this->wcmamtx_notices_settings_page; ?>[<?php echo $key; ?>][icon_source]"  value="default" <?php if ($icon_source == "custom") { echo 'checked'; } ?> disabled>
+     <label class="form-check-label wcmamtx_icon_checkbox_label">
+        <?php  echo esc_html__('Font Awesome Icon','customize-my-account-for-woocommerce'); ?>
+        <a href="#" data-toggle="modal" data-target="#wcmamtx_upgrade_modal">
+            <span class="wcmamtx_pro_upgrade_text"><?php  echo esc_html__('Upgrade to pro','customize-my-account-for-woocommerce'); ?></span>
 
-                            <div class="form-check wcmamtx_icon_checkbox">
-                                <input class="form-check-input wcmamtx_icon_source_radio" type="radio" name="<?php  echo esc_html__($this->wcmamtx_notices_settings_page); ?>[<?php echo $key; ?>][icon_source]"  value="upload" <?php if ($icon_source == "upload") { echo 'checked'; } ?>>
-                                <label class="form-check-label wcmamtx_icon_checkbox_label">
-                                    <?php  echo esc_html__('Upload Icon','customize-my-account-for-woocommerce'); ?>
-                                </label>
-                            </div>
-                        </div>
-                    </td>
-            
-                </tr>
+        </a>
+    </label>
+</div>
+
+<div class="form-check wcmamtx_icon_checkbox">
+ <input class="form-check-input wcmamtx_icon_source_radio" type="radio" name="<?php  echo $this->wcmamtx_notices_settings_page; ?>[<?php echo $key; ?>][icon_source]"  value="default" <?php if ($icon_source == "dashicon") { echo 'checked'; } ?> disabled>
+ <label class="form-check-label wcmamtx_icon_checkbox_label">
+    <?php  echo esc_html__('Dashicon','customize-my-account-for-woocommerce'); ?>
+    <a href="#" data-toggle="modal" data-target="#wcmamtx_upgrade_modal">
+        <span class="wcmamtx_pro_upgrade_text"><?php  echo esc_html__('Upgrade to pro','customize-my-account-for-woocommerce'); ?></span>
+
+    </a>
+</label>
+</div>
+
+<div class="form-check wcmamtx_icon_checkbox">
+    <input class="form-check-input wcmamtx_icon_source_radio" type="radio" name="<?php  echo $this->wcmamtx_notices_settings_page; ?>[<?php echo $key; ?>][icon_source]"  value="default" <?php if ($icon_source == "upload") { echo 'checked'; } ?> disabled>
+    <label class="form-check-label wcmamtx_icon_checkbox_label">
+        <?php  echo esc_html__('Upload Icon','customize-my-account-for-woocommerce'); ?>
+        <a href="#" data-toggle="modal" data-target="#wcmamtx_upgrade_modal">
+            <span class="wcmamtx_pro_upgrade_text"><?php  echo esc_html__('Upgrade to pro','customize-my-account-for-woocommerce'); ?></span>
+
+        </a>
+    </label>
+</div>
+</div>
+</td>
+
+</tr>
 
                 <tr class="fa_icon_tr" style= "<?php if ($icon_source == "custom") { echo 'display:table-row;'; } else { echo 'display:none;'; } ?>">
                     <td>
