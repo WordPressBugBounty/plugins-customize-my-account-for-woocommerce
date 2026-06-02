4474863e9390a02ca66ef9a9ccab845a03ccbfcb
Sean Murphy <sean@iamseanmurphy.com>
2026-06-02 21:16:40 +0000

    4.1.13

--- customize-my-account-for-woocommerce/customize-my-account-for-woocommerce.php
+++ customize-my-account-for-woocommerce/customize-my-account-for-woocommerce.php
@@ -3,7 +3,7 @@
     Plugin Name: SysBasics Customize My Account for WooCommerce
     Plugin URI: https://sysbasics.com
     Description: Customize My account page. Add/Edit/Remove Endpoints.
-    Version: 4.1.12
+    Version: 4.1.13
     Author: SysBasics
     Author URI: https://sysbasics.com
     Domain Path: /languages
